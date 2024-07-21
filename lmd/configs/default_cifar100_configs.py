f654f1848a958ddb7e56f2f563b87722476af4b8
Hong <hooong.yang@gmail.com>
2024-07-21 17:42:20 -0400

    fixing oom when restoring checkpoint if datasets are TF to Torch style

--- lmd/configs/default_cifar100_configs.py
+++ lmd/configs/default_cifar100_configs.py
@@ -7,7 +7,7 @@ def get_default_configs():
   # training
   config.training = training = ml_collections.ConfigDict()
   config.training.batch_size = 128
-  training.n_iters = 210001
+  training.n_iters = 1300001
   training.snapshot_freq = 10000
   training.log_freq = 50
   training.eval_freq = 100
